aa877da5a92bf87335a577efb8c2cd3892f5e843
valdofal <v.dofal@gmail.com>
2020-11-30 13:49:26 +0100

    'index'

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./components/App"
+class App extends React.Component{
+    render(){
+        return (
+            <div><p> Bonjour</p></div>
+        )
+    }
+}
+ReactDOM.render(<App />, document.getElementById("root"))
