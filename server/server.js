58d18dab7ffb4aee4e62d5a954b8ecb8f61d90a9
valdofal <v.dofal@gmail.com>
2020-11-30 10:53:45 +0100

    index.html

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const port = process.env.PORT || 5454;
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
-    res.send(index);
+    res.sendFile('C:/Users/Vdofa/Desktop/S5/NODEJS/pokemon/index.html',(err) => {
+        if (err) console.log('erreur')
+    });
 });
 
 app.listen(port, () => {
     console.log('Server app listening on port ' + port);
-});
