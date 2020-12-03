7ff075dbba2b168ba7d3301a5a8fcd505f830c0e
valdofal <v.dofal@gmail.com>
2020-12-03 15:21:39 +0100

    pokemonsToDB now adds all the attributes correctly for every pokemon

--- server/seed/hasAttack.js
+++ server/seed/hasAttack.js
@@ -13,8 +13,8 @@ let attacks = []
 knex.schema.dropTableIfExists('hasattack').then(function(){
     return knex.schema.createTable('hasattack', function(t) {
         t.increments().primary()
-        t.integer('pokemon_id')
-        t.foreign('pokemon_id').references('pokemons.id_pokemon')
+        t.integer('numéro')
+        t.foreign('numéro').references('pokemons.numéro')
         t.string('pokemon_name', 100)
         t.integer('attack_id')
         t.foreign('attack_id').references('id_attaque')
@@ -26,7 +26,7 @@ knex.schema.dropTableIfExists('hasattack').then(function(){
     for(let i = 0; i< file.length;i++){
         for(let j = 0; j < file[i].attaques.length; j ++){
             knex('hasattack').insert({
-                pokemon_id : file[i].numéro,
+                numéro : file[i].numéro,
                 pokemon_name : file[i].nom,
                 attack_name : file[i].attaques[j].nom,
                 attack_id : knex('attaques').where({nom : file[i].attaques[j].nom}).select('id_attaque')
--- server/seed/pokemonsToDB.js
+++ server/seed/pokemonsToDB.js
@@ -33,66 +33,27 @@ function getKeys (){
     })
     return keys
 }
+const keys = getKeys()
+
 
 knex.schema.dropTableIfExists('pokemons').then(function(){
     return knex.schema.createTable('pokemons',function(t) {
-        t.integer('id_pokemon').primary()
-        t.string('nom', 100)
-        t.string('nomen', 100)
-        t.string('nomja', 100)
-        t.string('nomtm', 100)
-        t.string('nomde', 100)
-        t.string('légende', 100)
-        t.integer('ndex')
-        t.integer('jdex')
-        t.integer('hdex')
-        t.integer('fdex')
-        t.string('odex', 100)
-        t.string('opdex', 100)
-        t.string('espece', 100)
-        t.float('taille')
-        t.float('poids')
-        t.float('fmratio')
-        t.integer('oeufpas')
-        t.string('effortval', 100)
-        t.string('type1', 100)
-        t.string('type2', 100)
-        t.integer('expval')
-        t.string('expmax', 100)
-        t.integer('captureval')
-        t.string('groupoeuf1', 100)
-        t.string('groupoeuf2', 100)
-        t.string('capspe1', 100)
-        t.string('capspe2', 100)
-        t.string('capspe2-reve', 100)
-        t.string('couleur', 100)
-        t.integer('forme')
-        t.string('pokemon', 100)
+        t.integer('numéro').primary()
+        for(let i = 1; i<keys.length; i++){
+            t.string(keys[i],300)
+        }
     })
 })
-
-
-
 .then(() => {
     for(let i = 0; i < file.length; i++){
-        knex('pokemons').insert({id_pokemon : file[i].numéro, nom : file[i].nom, nomen : file[i].nomen }).then(() => {
+        knex('pokemons').insert({numéro : file[i].numéro, nom : file[i].nom, nomen : file[i].nomen }).then(() => {
             console.log(`${file[i].nom} added`)
         })
+        for(let j = 0; j < keys.length; j++){
+            if (file[i][keys[j]] != undefined){
+                knex('pokemons').where({numéro : file[i].numéro}).update({[keys[j]] : file[i][keys[j]]}).then(() =>{
+                })
+            }
+        }
     }
 })
-
-
-// .then(() => {
-//     const keys = getKeys()
-//     let key = keys[0]
-//     for(let i = 0; i < file.length - 1; i++){
-//         knex('pokemons').insert({key: file[i].numéro, nom : file[i].nom }).then(() => {
-//             console.log('worked')
-//         })
-//         for(let j = 0; j < file.length - 1; j++)
-//             key = keys[j]
-//             knex('pokemons').where({id_pokemon : i}).update({keys: file[i].numéro, nom : file[i].nom }).then(() => {
-//                 console.log('worked')
-//             })
-//     }
-// })
